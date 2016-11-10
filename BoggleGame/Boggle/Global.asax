0f3fc7120f466a4d40f087b84e5e96637fae81e1
Moreno B <moreno_bralts@hotmail.com>
2016-11-10 16:36:02 +0100

    BoggleGame project will be redone in Angular in this branch. First Cleanup commit

--- BoggleGame/Boggle/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Boggle.WebApiApplication" Language="C#" %>
---
+++ BoggleGame/Boggle/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Boggle will use Angular!</h1>`
+})
+export class AppComponent { }
