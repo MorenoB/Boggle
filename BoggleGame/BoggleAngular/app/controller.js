bf0f88a9b87d8e9a22aae0189353e761d2158d6d
Moreno B <moreno_bralts@hotmail.com>
2016-11-14 15:37:20 +0100

    Added basic boggle box layout generation in Angular

---
+++ BoggleGame/BoggleAngular/app/controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('controller', controller);
+
+    controller.$inject = ['$scope', '$compile'];
+
+    function controller($scope, $compile) {
+        $scope.title = 'controller';
+
+        $scope.name = 'World';
+
+        $scope.addButton = function () {
+            alert("button clicked");
+            var btnhtml = '<button type="button" ng-click="addButton()">Click Me</button>';
+            var temp = $compile(btnhtml)($scope);
+            angular.element(document.getElementById('foo')).append(temp);
+        };
+        
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ BoggleGame/BoggleAngular/app/main.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Main', main);
+
+    main.$inject = ['$scope', '$compile'];
+
+    function main($scope, $compile) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.boggleWord = 'test';
+
+        activate();
+
+        function initBoard()
+        {
+            var columns = 4;
+            var rows = 4;
+            var rowElement = document.getElementById('row');
+            var boggleAreaElement = document.getElementById('boggleBoxArea');
+
+            for (var j = 0; j < rows; j++) {
+
+                var rowObject = angular.element('<li id="row"></li>');
+
+                for (var i = 0; i < columns; i++) {
+                    var btnhtml = '<button type="button" ng-click="addButton()">Letter</button>';
+                    var temp = $compile(btnhtml)($scope);
+                    angular.element(rowObject).append(temp);
+                }
+
+                angular.element(boggleAreaElement).append(rowObject);
+
+                
+            }
+
+
+        }
+
+        function activate()
+        {
+            initBoard();
+        }
+    }
+})();
